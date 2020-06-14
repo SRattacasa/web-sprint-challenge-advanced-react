115c58c0dc7b676e7b87ed61d18c25d5e6216fa4
SRattacasa <38533+SRattacasa@users.noreply.github.com>
2020-06-14 19:40:27 -0400

    Got plants to render

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,1 +1,43 @@
+import { useState } from "react";
+
 // write your custom hook here to control your checkout form
+ const useForm = (initialValues) => {
+    // inputs - handleChange, state (dynamically manage any number of inputs)
+    const [setValues, values] = useState(initialValues);
+  
+    const handleChanges = e => {
+      console.log(e.target.name);
+      setValues({
+        ...values,
+        [e.target.name]: e.target.value
+      });
+    };
+  
+    // form/buttons - submit, clear
+    const handleSubmit = e => {
+      if (e) e.preventDefault();
+      
+    };
+  
+    const clearForm = e => {
+      e.preventDefault();
+      setValues(initialValues);
+    };
+  
+    return [values, clearForm, handleSubmit, handleChanges];
+  };
+  
+  // Protip:
+  /*
+  
+  this function can control multiple inputs in a class component:
+  
+    const handleChanges = e => {
+      console.log(e.target.name);
+      this.setState({
+        [e.target.name]: e.target.value
+      });
+    };
+  
+  */
+  export default useForm;
